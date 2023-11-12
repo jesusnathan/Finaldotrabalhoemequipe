3eba48ab5ddc850775acf16e0bfa0ec6ea83237f
franciscomateusrodriguescastro <150175920+franciscomateusrodriguescastro@users.noreply.github.com>
2023-11-12 16:27:26 -0300

    atualização

--- src/App.java
+++ src/App.java
@@ -5,5 +5,6 @@ public class App {
         System.out.println("Alô MUNDO!!!");
         System.out.println("Alô MUNDO!!!");
         System.out.println("Alô MUNDO!!!");
+        System.out.println("Alô MUNDO!!!");
     }
 }
