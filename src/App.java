dc7a11060c9ad99b2c2dd2b88b04097f9953bfc9
franciscomateusrodriguescastro <150175920+franciscomateusrodriguescastro@users.noreply.github.com>
2023-11-12 15:52:17 -0300

    App.java

--- src/App.java
+++ src/App.java
@@ -2,7 +2,5 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Alô MUNDO!!!");       
         System.out.println("Tudo bem com você??");
-
-        
     }
 }
