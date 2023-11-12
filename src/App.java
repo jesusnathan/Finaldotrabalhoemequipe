52ff0907d4f2ee8ac47027cb556961f813c428b3
franciscomateusrodriguescastro <150175920+franciscomateusrodriguescastro@users.noreply.github.com>
2023-11-12 16:33:33 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -4,5 +4,6 @@ public class App {
         System.out.println("Hello Brother");
         System.out.println("Alô MUNDO!!!");
         System.out.println("Deu certo a primeira parte do trabalho");
+        https://github.com/franciscomateusrodriguescastro/Finaldotrabalhoemequipe/blob/main/src/App.java
     }
 }
