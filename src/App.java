73143320dd10b27aa3be92971da783120a0f58f8
Mateus Rodrigues <matesrodrigues@gmai.com>
2023-11-12 16:01:55 -0300

    Teste

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Alô MUNDO!!!");       
+        System.out.println("Alô MUNDO!!!"); 
+        System.out.println("Hello Brother");
     }
 }
