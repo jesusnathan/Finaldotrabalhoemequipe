bab289afe5dc31c9f6e45bc65fe7175d134deba9
Mateus Rodrigues <matesrodrigues@gmai.com>
2023-11-13 14:01:38 -0300

    atualização do app.java

--- src/App.java
+++ src/App.java
@@ -1,22 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
-       Personagem arqueiro = new Personagem();
-       arqueiro.nome = "Tiro Certo";
-       arqueiro.nivel = 3;
-       arqueiro.forca = 18;
+        Personagem arqueiro = new Personagem();
+        arqueiro.nome = "Tiro Certo";
+        arqueiro.nivel = 3;
+        arqueiro.forca = 18;
 
-      arqueiro.mostrarStatus();
-      arqueiro.atacar();
+        arqueiro.mostrarStatus();
+        arqueiro.atacar();
     }
 }
-public class Personagem {
+
+class Personagem {
     String nome;
     int nivel;
     int forca;
-}
+
     void mostrarStatus() {
-        System.out.format("Personagem: %s (lvl %d) com %d força.",nome, nivel, forca);
+        System.out.format("Personagem: %s (lvl %d) com %d força. ", nome, nivel, forca);
     }
-    void atacar(){
-        System.out.format("%s atacou e retirou %d de dano.",nome ,forca);
+
+    void atacar() {
+        System.out.format("%s atacou e retirou %d de dano.", nome, forca);
     }
+}
