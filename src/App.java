eca8b3bc8e6b1bd47164da79f441946c1a9f7ba8
Francisco Gutemberg Silva <132959951+OGutembergSilva@users.noreply.github.com>
2023-11-12 22:49:25 -0300

    Update App.java
    Isso foi o que eu criei,veja se roda na sua maquina.

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
-public class App {
+
+//public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Alô MUNDO!!!"); 
         System.out.println("Hello Brother");
@@ -6,3 +7,25 @@ public class App {
         System.out.println("Deu certo a primeira parte do trabalho");
     }
 }
+public class App {
+    public static void main(String[] args) throws Exception {
+       Personagem arqueiro = new Personagem();
+       arqueiro.nome = "Tiro Certo";
+       arqueiro.nivel = 3;
+       arqueiro.forca = 18;
+
+      arqueiro.mostrarStatus();
+      arqueiro.atacar();
+    }
+}
+public class Personagem {
+    String nome;
+    int nivel;
+    int forca;
+
+    void mostrarStatus() {
+        System.out.format("Personagem: %s (lvl %d) com %d força.",nome, nivel, forca);
+    }
+    void atacar(){
+        System.out.format("%s atacou e retirou %d de dano.",nome ,forca);
+    }
