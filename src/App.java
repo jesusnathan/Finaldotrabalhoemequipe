12035190346c071267941be135239dc74ac10aaf
franciscomateusrodriguescastro <150175920+franciscomateusrodriguescastro@users.noreply.github.com>
2023-11-15 14:45:46 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,9 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Personagem arqueiro = new Personagem();
-        arqueiro.nome = "Gojo";
-        arqueiro.nivel = 15;
-        arqueiro.forca = 17;
+        arqueiro.nome = "Tiro Certo";
+        arqueiro.nivel = 3;
+        arqueiro.forca = 18;
 
         arqueiro.mostrarStatus();
         arqueiro.atacar();
@@ -23,3 +23,4 @@ class Personagem {
         System.out.format("%s atacou e retirou %d de dano.", nome, forca);
     }
 }
+
